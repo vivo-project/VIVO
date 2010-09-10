a83ebecc364e80dc8ae0c7146de7ce3e64341c8a
rjy7 <rjy7@43c2fc9b-1942-4403-87b2-35b4218f7473>
2010-09-10 14:23:58 +0000

    NIHVIVO-650 Reworked Freemarker sparkline implementation for better controller/view separation

--- productMods/js/visualization/sparkline.js
+++ productMods/js/visualization/sparkline.js
@@ -3,20 +3,28 @@
 /* Javascript for sparkline visualization on person profile page */
 
 var visualization = {
-	render: function(visualizationData) {
-		var container = $('#' + visualizationData.container);
-        //$(container).empty().html('<img src="${loadingImageLink}" />');
+	render: function(url) {
+		var containerId = 'vis_container',
+		    container = $('#' + containerId);
+			
+        //container.empty().html('<img src="${loadingImageLink}" />');
        
         $.ajax({
-            url: visualizationData.url,
-            dataType: "html",
+            url: url,
+			data: {
+				'render_mode': 'dynamic',
+				'vis': 'person_pub_count',
+				'vis_mode': 'short',
+				'container': containerId
+			},
+            dataType: 'html',
             success:function(data){
-                $(container).html(data);
+                container.html(data);
             }
         });		
 	}
 };
 
 $(document).ready(function() {
-    visualization.render(visualizationData);
+    visualization.render(visualizationUrl);
 });
--- productMods/templates/freemarker/body/partials/individual/individual-sparklineVisualization.ftl
+++ productMods/templates/freemarker/body/partials/individual/individual-sparklineVisualization.ftl
@@ -2,19 +2,13 @@
 
 <#-- Template for sparkline visualization on individual profile page -->
 
-<#if visualization.url??>
+<#if visualizationUrl??>
     <div id="vis_container">&nbsp;</div>
-    
-    <#assign visContainer = "vis_container">
-    <#assign visUrl = "${visualization.url}&container=${visContainer}">
     
     ${stylesheets.addFromTheme("/visualization/visualization.css")}
     ${scripts.add("/js/visualization/sparkline.js")}
     
     <script type="text/javascript">
-    var visualizationData = {
-        url: '${visUrl}',
-        container: '${visContainer}'
-    };
+        var visualizationUrl = '${visualizationUrl}';
     </script>
 </#if>
