4d240f97d50b1495314e928b9c04d70e3ea17b0e
mb863 <mb863@43c2fc9b-1942-4403-87b2-35b4218f7473>
2011-03-14 19:20:33 +0000

    NIHVIVO-2163: Added markup and styles for verbose mode in individual admin panel.

--- productMods/js/individual/individualUtils.js
+++ productMods/js/individual/individualUtils.js
@@ -5,4 +5,6 @@ $(document).ready(function(){
     // "more"/"less" HTML truncator for showing more or less content in data property core:overview
     $('.overview-value').truncate({max_length: 500});
     
+    // Change background color button when verbose mode is off
+    $('#verbosePropertyForm input#submit[value="Turn off"]').addClass("verbose-off")
 });
