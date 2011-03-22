903330afad91c7d642fe1655ca700581246dc074
manolobevia <manolobevia@43c2fc9b-1942-4403-87b2-35b4218f7473>
2011-03-22 03:30:45 +0000

    NIHVIVO-2163: refactor markup and styles for verbose property display for individual page

--- productMods/js/individual/individualUtils.js
+++ productMods/js/individual/individualUtils.js
@@ -6,5 +6,5 @@ $(document).ready(function(){
     $('.overview-value').truncate({max_length: 500});
     
     // Change background color button when verbose mode is off
-    $('#verbosePropertyForm input#submit[value="Turn off"]').addClass("verbose-off")
+    $('a#verbosePropertySwitch:contains("Turn off")').addClass('verbose-off');
 });
