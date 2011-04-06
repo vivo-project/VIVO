6a2614710f86ba39c5b99cddd1db80d0aa4d4be1
mbarbier <mbarbier@43c2fc9b-1942-4403-87b2-35b4218f7473>
2011-04-06 20:03:34 +0000

    web.xml: add servlet for AboutQrCodesController aboutQrCodes.ftl: create initial mockup foaf-person--exportQrCode.ftl: rename from individual-exportQrCode individual-exportQrCode.ftl: delete AboutQrCodesController.java: create ExportQrCodeController.java: handle rename of FTL file

--- src/edu/cornell/mannlib/vitro/webapp/controller/AboutQrCodesController.java
+++ src/edu/cornell/mannlib/vitro/webapp/controller/AboutQrCodesController.java
@@ -8,33 +8,22 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import edu.cornell.mannlib.vitro.webapp.beans.Individual;
 import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
 import edu.cornell.mannlib.vitro.webapp.controller.freemarker.FreemarkerHttpServlet;
-import edu.cornell.mannlib.vitro.webapp.controller.freemarker.IndividualController;
 import edu.cornell.mannlib.vitro.webapp.controller.freemarker.responsevalues.ExceptionResponseValues;
 import edu.cornell.mannlib.vitro.webapp.controller.freemarker.responsevalues.ResponseValues;
 import edu.cornell.mannlib.vitro.webapp.controller.freemarker.responsevalues.TemplateResponseValues;
-import edu.cornell.mannlib.vitro.webapp.web.templatemodels.individual.IndividualTemplateModel;
-import freemarker.ext.beans.BeansWrapper;
-import freemarker.template.DefaultObjectWrapper;
 
-public class ExportQrCodeController extends FreemarkerHttpServlet {
+public class AboutQrCodesController extends FreemarkerHttpServlet {
     
     private static final long serialVersionUID = 1L;
     private static final Log log = LogFactory.getLog(ExportQrCodeController.class);
-    private static final String TEMPLATE_DEFAULT = "foaf-person--exportQrCode.ftl";
+    private static final String TEMPLATE_DEFAULT = "aboutQrCodes.ftl";
 
     @Override
     protected ResponseValues processRequest(VitroRequest vreq) {
         try {
-            Individual individual = IndividualController.getIndividualFromRequest(vreq);
-            
-            DefaultObjectWrapper wrapper = new DefaultObjectWrapper();
-            wrapper.setExposureLevel(BeansWrapper.EXPOSE_SAFE);
-            
             Map<String, Object> body = new HashMap<String, Object>();
-            body.put("individual", wrapper.wrap(new IndividualTemplateModel(individual, vreq)));
             
             return new TemplateResponseValues(TEMPLATE_DEFAULT, body);
         } catch (Throwable e) {
@@ -45,7 +34,7 @@ public class ExportQrCodeController extends FreemarkerHttpServlet {
 
     @Override
     protected String getTitle(String siteName, VitroRequest vreq) {
-        return "Export QR Code for " + IndividualController.getIndividualFromRequest(vreq).getRdfsLabel();
+        return "About QR Codes";
     }
 
 }
