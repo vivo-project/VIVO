923572053acb69f9b58b52a32543ec1b6f236cab
tworrall <tworrall@43c2fc9b-1942-4403-87b2-35b4218f7473>
2011-04-11 15:47:44 +0000

    NIHVIVO-2160:improved tooltip for uri icon via javascript

--- productMods/js/individual/individualUtils.js
+++ productMods/js/individual/individualUtils.js
@@ -7,4 +7,38 @@ $(document).ready(function(){
     
     // Change background color button when verbose mode is off
     $('a#verbosePropertySwitch:contains("Turn off")').addClass('verbose-off');
+
+    // This function creates and styles the "qTip" tooltip that displays the resource uri when the user clicks the uri icon.
+    $('#uriIcon').each(function()
+    {
+       $(this).qtip(
+       {
+          content: {
+              title: { 
+                  text: 'URI',
+              },
+              text: '<br /><span id="uri-text">' + $("#uriIcon").attr("title") + '</span><br /><br />'
+          },
+          position: {
+              corner: {
+              target: 'leftBottom',
+              tooltip: 'bottomLeft'
+              }
+          },
+          show: {
+             when: {event: 'click'}
+          },
+          hide: {
+             fixed: true // Make it fixed so it can be hovered over
+          },
+          style: {
+              title: {'backgroundColor': '#2485AE', 'color': 'white', 'fontFamily': '"Lucida Sans Unicode","Lucida Grande", Geneva, helvetica, sans-serif'},
+             padding: '6px 6px', // Give it some extra padding
+             width: 380,
+             textAlign: 'center',
+             backgroundColor: '#f1f2ee'
+         }
+       });
+    });
+    
 });
