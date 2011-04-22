26f1c622492167c086b16d21c85c663c67a230eb
mbarbier <mbarbier@43c2fc9b-1942-4403-87b2-35b4218f7473>
2011-04-22 20:15:07 +0000

    TestFileController.java: clean up a bit

--- src/edu/cornell/mannlib/vitro/webapp/controller/harvester/TestFileController.java
+++ src/edu/cornell/mannlib/vitro/webapp/controller/harvester/TestFileController.java
@@ -3,49 +3,27 @@
 package edu.cornell.mannlib.vitro.webapp.controller.harvester; 
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.hp.hpl.jena.ontology.OntModel;
-import com.hp.hpl.jena.ontology.OntModelSpec;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.ModelMaker;
-import com.hp.hpl.jena.shared.JenaException;
-import com.hp.hpl.jena.shared.Lock;
-
-import edu.cornell.mannlib.vedit.beans.LoginStatusBean;
-import edu.cornell.mannlib.vitro.webapp.beans.Portal;
-import edu.cornell.mannlib.vitro.webapp.controller.Controllers;
 import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
 import edu.cornell.mannlib.vitro.webapp.controller.freemarker.FreemarkerHttpServlet;
-import edu.cornell.mannlib.vitro.webapp.controller.freemarker.TemplateProcessingHelper.TemplateProcessingException;
 import edu.cornell.mannlib.vitro.webapp.controller.freemarker.responsevalues.ExceptionResponseValues;
 import edu.cornell.mannlib.vitro.webapp.controller.freemarker.responsevalues.ResponseValues;
 import edu.cornell.mannlib.vitro.webapp.controller.freemarker.responsevalues.TemplateResponseValues;
-import edu.cornell.mannlib.vitro.webapp.dao.WebappDaoFactory;
-import edu.cornell.mannlib.vitro.webapp.dao.jena.JenaModelUtils;
-import edu.cornell.mannlib.vitro.webapp.dao.jena.event.EditEvent;
 import edu.cornell.mannlib.vitro.webapp.filestorage.uploadrequest.FileUploadServletRequest;
-import freemarker.template.Configuration;
 
 public class TestFileController extends FreemarkerHttpServlet {
 
@@ -75,20 +53,6 @@ public class TestFileController extends FreemarkerHttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
 
-        if(1 == 1)
-        {
-            //Map<String, Object> body = new HashMap<String, Object>();
-            //body.put("uploadPostback", "true");
-            //body.put("testvalue", "hello!");
-            //ResponseValues responseValues = new TemplateResponseValues(TEMPLATE_DEFAULT, body);
-            //ResponseValues responseValues = new Re
-
-            //response.getWriter().write("hello!");
-
-            //try {doResponse(new VitroRequest(request), response, responseValues);} catch(Exception e) {log.error(e, e);}
-            //return;
-        }
-
         int maxFileSize = 1024 * 1024;
 
         FileUploadServletRequest req = FileUploadServletRequest.parseRequest(request, maxFileSize);
@@ -98,19 +62,6 @@ public class TestFileController extends FreemarkerHttpServlet {
 
         Map<String, List<FileItem>> fileStreams = req.getFiles();
 
-        VitroRequest vreq = new VitroRequest(req);       
-        //if (!checkLoginStatus(request,response) ){
-            //todo: complete
-        //}
-
-        //LoginStatusBean loginBean = LoginStatusBean.getBean(vreq);
-
-        //try {
-        //    super.doGet(vreq,response);
-        //} catch (Exception e) {
-            //todo: complete
-        //}
-
         JSONObject json = new JSONObject();
 
         String path = "/home/mbarbieri/tempfileupload/"; //todo: complete
@@ -118,16 +69,7 @@ public class TestFileController extends FreemarkerHttpServlet {
             FileItem csvStream = fileStreams.get("csvFile").get(0);
             String name = csvStream.getName();
             File file = new File(path + name);
-            //FileWriter writer = new FileWriter(file);
             try {
-                /*
-                InputStream inputStream = csvStream.getInputStream();
-
-                for(int nextByte = inputStream.read(); nextByte != -1; nextByte = inputStream.read())
-                {
-                    writer.write(nextByte);
-                }
-                */
                 csvStream.write(file);
             } catch (Exception e) {                               
                 //todo: handle
@@ -167,13 +109,6 @@ public class TestFileController extends FreemarkerHttpServlet {
         }
 
         response.getWriter().write(json.toString());
-
-        /*
-         * Ajax request: filename, data
-         * Process: validate
-         * Ajax response: success or error message
-         * 
-         */
     }
 
     private String validateCsvFile(File file)
