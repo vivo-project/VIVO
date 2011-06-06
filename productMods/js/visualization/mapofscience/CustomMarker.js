c763ac1bfa67bdb3725ecccffe5dfac5e7be7d66
kongchinhua <kongchinhua@43c2fc9b-1942-4403-87b2-35b4218f7473>
2011-06-06 18:41:45 +0000

    Improve visibility of the overlap nodes

--- productMods/js/visualization/mapofscience/CustomMarker.js
+++ productMods/js/visualization/mapofscience/CustomMarker.js
@@ -48,11 +48,11 @@ function createScinodeMarker(map, label, value, radius, color, latlng) {
 	var circleOptions = {
 		label: label,
 		value: value,
-		strokeColor: color,
+		strokeColor: '#000',
 		strokeOpacity: 1.0,
 		strokeWeight: 1.0,
 		fillColor: color,
-		fillOpacity: 0.25,
+		fillOpacity: 0.9,
 		map: map,
 		center: latlng,
 		zIndex: -radius,
--- productMods/js/visualization/mapofscience/SizeCoding.js
+++ productMods/js/visualization/mapofscience/SizeCoding.js
@@ -56,8 +56,8 @@ var CircleSizeCoder = Class.extend({
 		this.options = $.extend({}, this.options, options);
 	},
 	options: {
-		minRadius: 0.8,
-		maxRadius: 100.0,
+		minRadius: 0,
+		maxRadius: 25.0,
 		scaler: new Scaler({})
 	},
 	getSize: function(value) {
