f93b0259637686895f0763eef0b5f1aee6d20cff
brianjlowe <brianjlowe@43c2fc9b-1942-4403-87b2-35b4218f7473>
2011-10-26 21:08:54 +0000

    NIHVIVO-3223 derive dc:creator for documents

---
+++ src/org/vivoweb/reasoner/plugin/DCCreatorForDocuments.java
@@ -0,0 +1,16 @@
+package org.vivoweb.reasoner.plugin;
+
+import edu.cornell.mannlib.vitro.webapp.reasoner.ReasonerPlugin;
+
+public class DCCreatorForDocuments extends SimpleBridgingRule implements ReasonerPlugin {
+	
+	private final static String DCTERMS = "http://purl.org/dc/terms/";
+	private final static String VIVOCORE = "http://vivoweb.org/ontology/core#";
+	
+	public DCCreatorForDocuments() {
+		super(VIVOCORE + "informationResourceInAuthorship", 
+			  VIVOCORE + "linkedAuthor",
+			  DCTERMS + "creator");
+	}
+
+}
--- src/org/vivoweb/reasoner/plugin/DCTitleForDocuments.java
+++ src/org/vivoweb/reasoner/plugin/DCTitleForDocuments.java
@@ -16,102 +16,12 @@ import com.hp.hpl.jena.vocabulary.RDFS;
 
 import edu.cornell.mannlib.vitro.webapp.reasoner.ReasonerPlugin;
 
-public class DCTitleForDocuments implements ReasonerPlugin {
+public class DCTitleForDocuments extends SimplePropertyAndTypeRule implements ReasonerPlugin {
 
-	private final static Log log = LogFactory.getLog(DCTitleForDocuments.class);
-	
-	private final Resource BIBO_DOCUMENT = ResourceFactory.createResource(
-			                        "http://purl.org/ontology/bibo/Document");
-	private final Property DCTERMS_TITLE = ResourceFactory.createProperty(
-			                        "http://purl.org/dc/terms/title");
-		   
-	public boolean isInterestedInAddedStatement(Statement stmt) {
-		return (isRelevantType(stmt) || isRelevantPredicate(stmt));
+	public DCTitleForDocuments() {
+		super(RDFS.label.getURI(), 
+		      "http://purl.org/ontology/bibo/Document", 
+		      "http://purl.org/dc/terms/title");
 	}
-	
-	public boolean isInterestedInRemovedStatement(Statement stmt) {
-		return (isRelevantType(stmt) || isRelevantPredicate(stmt));
-	}
-	
-	public void addedABoxStatement(Statement stmt, 
-            Model aboxAssertionsModel, 
-            Model aboxInferencesModel, 
-            OntModel TBoxInferencesModel) {
-		boolean relevantType = isRelevantType(stmt);
-		boolean relevantPredicate = isRelevantPredicate(stmt);
-
-		if (relevantType) {
-			StmtIterator stmtIt = aboxAssertionsModel.listStatements(
-					stmt.getSubject(), RDFS.label, (RDFNode)null);
-			while (stmtIt.hasNext()) {
-				Statement s = stmtIt.nextStatement();
-				tryToInfer(stmt.getSubject(), 
-						   DCTERMS_TITLE, 
-						   s.getObject(), 
-						   aboxAssertionsModel, 
-						   aboxInferencesModel);
-			}
-		} else if (relevantPredicate) {
-			if(aboxAssertionsModel.contains(
-					stmt.getSubject(), RDF.type, BIBO_DOCUMENT) 
-			  || aboxInferencesModel.contains(
-					  stmt.getSubject(), RDF.type, BIBO_DOCUMENT)) {
-				tryToInfer(stmt.getSubject(), 
-						   DCTERMS_TITLE, 
-						   stmt.getObject(), 
-						   aboxAssertionsModel, 
-						   aboxInferencesModel);
-			}
-		}
-	}
-	
-	private void tryToInfer(Resource subject, 
-			                Property predicate, 
-			                RDFNode object, 
-			                Model aboxAssertionsModel, 
-			                Model aboxInferencesModel) {
-		// this should be part of a superclass or some class that provides
-		// reasoning framework functions
-		Statement s = ResourceFactory.createStatement(subject, predicate, object);
-		if (!aboxAssertionsModel.contains(s) && !aboxInferencesModel.contains(s)) {
-			aboxInferencesModel.add(s);
-		}
-	}
-
-    public void removedABoxStatement(Statement stmt, 
-            Model aboxAssertionsModel, 
-            Model aboxInferencesModel, 
-            OntModel TBoxInferencesModel) {
-    	
-    	if (isRelevantPredicate(stmt)) {
-//    		if (aboxAssertionsModel.contains(
-//    				stmt.getSubject(), RDF.type, BIBO_DOCUMENT)
-//    			        || aboxInferencesModel.contains(
-//    						stmt.getSubject(), RDF.type, BIBO_DOCUMENT)) {
-    			aboxInferencesModel.remove(
-    					stmt.getSubject(), DCTERMS_TITLE, stmt.getObject());
-//    		}
-    	} else if (isRelevantType(stmt)) {
-    		if(!aboxInferencesModel.contains(
-    				stmt.getSubject(), RDF.type, BIBO_DOCUMENT)) {
-    			StmtIterator labelIt = aboxAssertionsModel.listStatements(
-    					stmt.getSubject(), RDFS.label, (RDFNode) null);
-    			while (labelIt.hasNext()) {
-    				Statement labelStmt = labelIt.nextStatement();
-    				aboxInferencesModel.remove(
-    						labelStmt.getSubject(), DCTERMS_TITLE, labelStmt.getObject());
-    			}
-    		}
-    	}
-    }
-    
-    private boolean isRelevantType(Statement stmt) {
-		return (RDF.type.equals(stmt.getPredicate()) 
-				&& BIBO_DOCUMENT.equals(stmt.getObject()));
-    }	
-	
-    private boolean isRelevantPredicate(Statement stmt) {
-		return (RDFS.label.equals(stmt.getPredicate()));
-    }
-	
+			   
 }
