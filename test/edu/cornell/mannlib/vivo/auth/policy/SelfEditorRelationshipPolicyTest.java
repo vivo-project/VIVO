ed9c404197b42581759898bd12d32ff23cf3fee3
j2blake <j2blake@43c2fc9b-1942-4403-87b2-35b4218f7473>
2011-10-31 14:01:58 +0000

    NIHVIVO-2343 Fix unit tests to use new constructor on HasAssociatedIndividual

--- test/edu/cornell/mannlib/vivo/auth/policy/SelfEditorRelationshipPolicyTest.java
+++ test/edu/cornell/mannlib/vivo/auth/policy/SelfEditorRelationshipPolicyTest.java
@@ -30,6 +30,7 @@ import edu.cornell.mannlib.vitro.testing.AbstractTestClass;
 import edu.cornell.mannlib.vitro.webapp.auth.identifier.ArrayIdentifierBundle;
 import edu.cornell.mannlib.vitro.webapp.auth.identifier.IdentifierBundle;
 import edu.cornell.mannlib.vitro.webapp.auth.identifier.common.HasAssociatedIndividual;
+import edu.cornell.mannlib.vitro.webapp.auth.identifier.common.HasAssociatedIndividual.Mechanism;
 import edu.cornell.mannlib.vitro.webapp.auth.policy.bean.PropertyRestrictionPolicyHelper;
 import edu.cornell.mannlib.vitro.webapp.auth.policy.ifaces.Authorization;
 import edu.cornell.mannlib.vitro.webapp.auth.policy.ifaces.PolicyDecision;
@@ -420,7 +421,7 @@ public class SelfEditorRelationshipPolicyTest extends AbstractTestClass {
 	// ----------------------------------------------------------------------
 
 	private HasAssociatedIndividual makeSelfEditingId(String uri) {
-		return new HasAssociatedIndividual(uri);
+		return new HasAssociatedIndividual(uri, Mechanism.SELF);
 	}
 
 	private void assertDecision(Authorization expected, PolicyDecision decision) {
