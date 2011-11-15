1ff3a4963f21c0c9eaf140bb345985b3c5cb4699
briancaruso <briancaruso@43c2fc9b-1942-4403-87b2-35b4218f7473>
2011-11-15 22:05:43 +0000

    Working on default data process form

---
+++ src/edu/cornell/mannlib/vitro/webapp/edit/n3editing/N3TransitionToV2Mapping.java
@@ -0,0 +1,70 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+package edu.cornell.mannlib.vitro.webapp.edit.n3editing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class N3TransitionToV2Mapping extends HashMap<String, String>{        
+    public N3TransitionToV2Mapping(){
+        Map<String,String> map = this;
+        
+        // vivo forms:
+
+        map.put("addAuthorsToInformationResource.jsp",                
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddAuthorsToInformationResourceGenerator.class.getName());
+        map.put("manageWebpagesForIndividual.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.ManageWebpagesForIndividualGenerator.class.getName());
+        map.put("newIndividualForm.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.NewIndividualFormGenerator.class.getName());
+        map.put("organizationHasPositionHistory.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.OrganizationHasPositionHistoryGenerator.class.getName());
+        map.put("personHasEducationalTraining.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.PersonHasEducationalTraining.class.getName());
+        map.put("personHasPositionHistory.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.PersonHasPositionHistoryGenerator.class.getName());                
+        map.put("addGrantRoleToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddGrantRoleToPersonGenerator.class.getName());
+        map.put("addEditWebpageForm.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddEditWebpageFormGenerator.class.getName());
+        map.put("addPublicationToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddPublicationToPersonGenerator.class.getName());
+
+//        map.put("terminologyAnnotation.jsp",
+//                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.TerminologyAnnotationGenerator.class.getName());
+//        
+//        map.put("redirectToPublication.jsp",
+//                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.RedirectToPublicationGenerator.class.getName());
+//        map.put("unsupportedBrowserMessage.jsp",
+//                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.UnsupportedBrowserMessage.class.getName());
+//        
+        // vivo 2 stage role forms:
+
+        map.put("addAttendeeRoleToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddAttendeeRoleToPersonGenerator.class.getName());
+        map.put("addClinicalRoleToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddClinicalRoleToPersonGenerator.class.getName());
+        map.put("addEditorRoleToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddEditorRoleToPersonGenerator.class.getName());
+        map.put("addHeadOfRoleToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddHeadOfRoleToPersonGenerator.class.getName());
+        map.put("addMemberRoleToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddMemberRoleToPersonGenerator.class.getName());
+        map.put("addOrganizerRoleToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddOrganizerRoleToPersonGenerator.class.getName());
+        map.put("addOutreachRoleToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddOutreachProviderRoleToPersonGenerator.class.getName());
+        map.put("addPresenterRoleToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddPresenterRoleToPersonGenerator.class.getName());                
+        map.put("addResearcherRoleToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddResearcherRoleToPersonGenerator.class.getName());
+        map.put("addReviewerRoleToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddReviewerRoleToPersonGenerator.class.getName());
+        map.put("addRoleToPersonTwoStage.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddRoleToPersonTwoStageGenerator.class.getName());
+        map.put("addServiceProviderRoleToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddServiceProviderRoleToPersonGenerator.class.getName());
+        map.put("addTeacherRoleToPerson.jsp",
+                edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators.AddTeacherRoleToPersonGenerator.class.getName());
+    
+    }
+}
---
+++ src/edu/cornell/mannlib/vitro/webapp/edit/n3editing/configuration/generators/AddAuthorsToInformationResourceGenerator.java
@@ -0,0 +1,19 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
+package edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators;
+
+import javax.servlet.http.HttpSession;
+
+import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
+import edu.cornell.mannlib.vitro.webapp.edit.n3editing.VTwo.EditConfigurationVTwo;
+
+public class AddAuthorsToInformationResourceGenerator extends BaseEditConfigurationGenerator implements EditConfigurationGenerator {
+
+    @Override
+    public EditConfigurationVTwo getEditConfiguration(VitroRequest vreq,
+            HttpSession session) {
+        // TODO Auto-generated method stub
+        throw new Error(this.getClass().getName() + " is not yet implement");
+    }
+
+}
