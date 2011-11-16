72f1ca28122bc273ad7e106f6d309d6117f2b28d
brianjlowe <brianjlowe@43c2fc9b-1942-4403-87b2-35b4218f7473>
2011-11-16 15:54:42 +0000

    license tag

--- src/edu/cornell/mannlib/vitro/webapp/edit/n3editing/configuration/generators/PersonHasPositionHistoryGenerator.java
+++ src/edu/cornell/mannlib/vitro/webapp/edit/n3editing/configuration/generators/PersonHasPositionHistoryGenerator.java
@@ -1,3 +1,5 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
 package edu.cornell.mannlib.vitro.webapp.edit.n3editing.configuration.generators;
 
 import java.util.Arrays;
