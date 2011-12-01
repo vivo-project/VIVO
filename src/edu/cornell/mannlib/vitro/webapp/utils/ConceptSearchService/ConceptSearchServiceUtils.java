556cd102f3a045e2c6eaf995b11d0daf6ed3d3e2
tworrall <tworrall@43c2fc9b-1942-4403-87b2-35b4218f7473>
2011-12-01 20:43:58 +0000

    added http:// to agrovoc link because it was trying to link to http://localhost:8080/vivo/...

--- src/edu/cornell/mannlib/vitro/webapp/utils/ConceptSearchService/ConceptSearchServiceUtils.java
+++ src/edu/cornell/mannlib/vitro/webapp/utils/ConceptSearchService/ConceptSearchServiceUtils.java
@@ -38,8 +38,8 @@ public class ConceptSearchServiceUtils {
 	public static HashMap<String, VocabSourceDescription> getVocabSources() {
 		HashMap<String, VocabSourceDescription> map = new HashMap<String, VocabSourceDescription>();
     	map.put(UMLSVocabSource, new VocabSourceDescription("UMLS", UMLSVocabSource, "http://www.nlm.nih.gov/research/umls/", "Unified Medical Language System"));
-    	map.put(AgrovocVocabSource, new VocabSourceDescription("AGROVOC", AgrovocVocabSource, "www.fao.org/agrovoc/", "Agricultural Vocabulary"));
-    	map.put(GemetVocabSource, new VocabSourceDescription("GEMET", GemetVocabSource, "http://www.eionet.europa.eu/gemet", "GEneral Multilingual Environmental Thesaurus"));
+    	map.put(AgrovocVocabSource, new VocabSourceDescription("AGROVOC", AgrovocVocabSource, "http://www.fao.org/agrovoc/", "Agricultural Vocabulary"));
+    	map.put(GemetVocabSource, new VocabSourceDescription("GEMET", GemetVocabSource, "http://www.eionet.europa.eu/gemet", "General Multilingual Environmental Thesaurus"));
     	return map;
 	}
 	
