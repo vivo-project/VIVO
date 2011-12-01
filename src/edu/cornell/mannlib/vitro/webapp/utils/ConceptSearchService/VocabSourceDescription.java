ff97b2d450c7d3d088bbd5c0ce56d2ac9a06123c
hjkhjk54 <hjkhjk54@43c2fc9b-1942-4403-87b2-35b4218f7473>
2011-12-01 19:47:30 +0000

    vocab source class

---
+++ src/edu/cornell/mannlib/vitro/webapp/utils/ConceptSearchService/VocabSourceDescription.java
@@ -0,0 +1,33 @@
+/* $This file is distributed under the terms of the license in /doc/license.txt$ */
+
+package edu.cornell.mannlib.vitro.webapp.utils.ConceptSearchService;
+
+public class VocabSourceDescription {
+	private String url;
+	private String label;
+	private String schema;
+	private String description;
+	
+	public VocabSourceDescription(String inputLabel, String inputSchema, String inputUrl, String inputDescription) {
+		url = inputUrl;
+		label = inputLabel;
+		schema = inputSchema;
+		description = inputDescription;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public String getLabel() {
+		return label;
+	}
+	
+	public String getSchema() {
+		return schema;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+}
