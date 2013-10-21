4f085bcdfe4d80a3991573cf071f2a37203239e6
j2blake <jeb228@cornell.edu>
2013-10-21 15:21:54 -0400

    Update the LoadTesting framework.
    Permit use of Git while still supporting Subversion. Improve the script structure and reduce dependence on hard-coded paths.

--- utilities/LoadTesting/scripts/subscripts/loadParms.rb
+++ utilities/LoadTesting/scripts/subscripts/loadParms.rb
@@ -1,6 +1,6 @@
-require "#{File.dirname(__FILE__)}/_current_version"
-require "#{File.dirname(__FILE__)}/_current_test"
-require "/home/jeb228/LoadTesting/versions/#{@version_name}/version_properties"
-require "/home/jeb228/LoadTesting/distros/#{@distro_name}/distro_properties"
-require "/home/jeb228/LoadTesting/sites/#{@site_name}/site_properties"
+require 'subscripts/_current_version'
+require 'subscripts/_current_test'
+require version_file('version_properties')
+require distro_file('distro_properties')
+require site_file('site_properties')
 
