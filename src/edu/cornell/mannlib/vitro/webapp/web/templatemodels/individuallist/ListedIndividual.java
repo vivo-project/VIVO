7bff226f00e598b2619617f4f415184830700d76
j2blake <jeb228@cornell.edu>
2013-10-28 11:58:00 -0400

    VIVO-396 Obtain preferred title from VCard

--- src/edu/cornell/mannlib/vitro/webapp/web/templatemodels/individuallist/ListedIndividual.java
+++ src/edu/cornell/mannlib/vitro/webapp/web/templatemodels/individuallist/ListedIndividual.java
@@ -2,28 +2,60 @@
 
 package edu.cornell.mannlib.vitro.webapp.web.templatemodels.individuallist;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+
 import edu.cornell.mannlib.vitro.webapp.beans.Individual;
 import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
-import edu.cornell.mannlib.vitro.webapp.web.templatemodels.individuallist.BaseListedIndividual;
-import edu.cornell.mannlib.vitro.webapp.web.templatemodels.individuallist.ListedIndividual;
+import edu.cornell.mannlib.vitro.webapp.dao.jena.QueryUtils;
 
 public class ListedIndividual extends BaseListedIndividual {
-
     private static final Log log = LogFactory.getLog(ListedIndividual.class);
 
-    private static final String CORE = "http://vivoweb.org/ontology/core#";
+    private static String VCARD_DATA_QUERY = ""
+            + "PREFIX obo: <http://purl.obolibrary.org/obo/> \n"
+            + "PREFIX vcard: <http://www.w3.org/2006/vcard/ns#>  \n"
+            + "SELECT DISTINCT ?title  \n"
+            + "WHERE {  \n"
+            + "    ?subject obo:ARG_2000028 ?vIndividual .  \n"
+            + "    ?vIndividual vcard:hasTitle ?vTitle . \n"
+            + "    ?vTitle vcard:title ?title . \n"
+            + "} "  ;
+
+    private final String title;
     
     public ListedIndividual(Individual individual, VitroRequest vreq) {
         super(individual, vreq);
+        title = findPreferredTitle();
     }
-    
+
+    private String findPreferredTitle() {
+        String queryStr = QueryUtils.subUriForQueryVar(VCARD_DATA_QUERY, "subject", individual.getURI());
+        log.debug("queryStr = " + queryStr);
+        String value = "";
+        try {
+            ResultSet results = QueryUtils.getQueryResults(queryStr, vreq);
+            while (results.hasNext()) {
+                QuerySolution soln = results.nextSolution();
+                String t = QueryUtils.nodeToString( soln.get("title"));
+                if (StringUtils.isNotBlank(t)) {
+                	value = t;
+                }
+            }
+        } catch (Exception e) {
+            log.error(e, e);
+        }
+        return value;
+    }
+
     /* Template properties */
     
     public String getPreferredTitle() {
-        return cleanTextForDisplay( individual.getDataValue(CORE + "preferredTitle") );
+        return title;
     }
     
 }
