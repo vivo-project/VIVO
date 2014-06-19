e411a2224e82bcd528d26515fbeb13f23c93d141
j2blake <jeb228@cornell.edu>
2014-06-19 16:18:13 -0400

    VIVO-795 Start a procedure for including docs in the release.

---
+++ utilities/releaseScripts/6_insert_documentation.rb
@@ -0,0 +1,15 @@
+=begin
+--------------------------------------------------------------------------------
+
+Create PDF versions of the Installation Instructions and the Upgrade Guide, 
+from the VIVONEXT Wiki space. 
+
+Store them in the docs directory.
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+=end
+puts "-------------------------------------------------------------------"
+puts "    Not automated yet. Get Installation and Upgrade PDFs and put   "
+puts "    them into the /doc directory.                                  "
+puts "-------------------------------------------------------------------"
