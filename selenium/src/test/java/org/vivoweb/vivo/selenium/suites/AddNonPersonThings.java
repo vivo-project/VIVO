d19d0f27c8fbbd1a025ff92ca495984d4712e1c7
Graham Triggs <gtriggs@duraspace.org>
2016-01-18 16:35:06 +0000

    Updates to the Selenium WebDriver tests.

--- selenium/src/test/java/org/vivoweb/vivo/selenium/suites/AddNonPersonThings.java
+++ selenium/src/test/java/org/vivoweb/vivo/selenium/suites/AddNonPersonThings.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
 import org.vivoweb.vivo.selenium.DriverFactory;
 import org.vivoweb.vivo.selenium.VIVOSuite;
+import org.vivoweb.vivo.selenium.tests.CheckPublicView;
 import org.vivoweb.vivo.selenium.tests.CreateActivity;
 import org.vivoweb.vivo.selenium.tests.CreateCourses;
 import org.vivoweb.vivo.selenium.tests.CreateEquipment;
@@ -21,6 +22,7 @@ import org.vivoweb.vivo.selenium.tests.DeleteLocations;
 import org.vivoweb.vivo.selenium.tests.DeleteOrganization;
 import org.vivoweb.vivo.selenium.tests.DeleteResearch;
 import org.vivoweb.vivo.selenium.tests.RebuildSearchIndex;
+import org.vivoweb.vivo.selenium.tests.VerifyAllThingsSearchable;
 
 @RunWith(VIVOSuite.class)
 @SuiteClasses(
@@ -33,6 +35,11 @@ import org.vivoweb.vivo.selenium.tests.RebuildSearchIndex;
                 CreateTopic.class,
                 CreateEquipment.class,
                 CreateLocation.class,
+                RebuildSearchIndex.class,
+                VerifyAllThingsSearchable.class,
+                CheckPublicView.class,
+//                CheckIndexView.class,
+//                TestMenuManagement.class,
                 DeleteActivities.class,
                 DeleteCourses.class,
                 DeleteLocations.class,
