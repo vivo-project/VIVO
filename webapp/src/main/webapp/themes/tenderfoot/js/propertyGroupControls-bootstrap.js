f6dd5a0a75ebc5a86895f59bbe56d1f797fa3787
Graham Triggs <gtriggs@duraspace.org>
2017-10-02 15:27:23 +0100

    Remove fade added by viewAll in tenderfoot

--- webapp/src/main/webapp/themes/tenderfoot/js/propertyGroupControls-bootstrap.js
+++ webapp/src/main/webapp/themes/tenderfoot/js/propertyGroupControls-bootstrap.js
@@ -38,6 +38,6 @@ $(document).ready(function(){
         $('[data-toggle="tab"]').parent().removeClass("active");
         $("#viewAllTab").parent().addClass("active");
         $("#viewAllTab").addClass("active");
-        $(".tab-pane").addClass("fade active in");
+        $(".tab-pane").addClass("active");
     }
 });
