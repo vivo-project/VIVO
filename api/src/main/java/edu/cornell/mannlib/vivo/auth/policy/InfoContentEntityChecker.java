b701a85457924cfe925a443b309b5bf200f7d2f9
Graham Triggs <graham.triggs@tib.eu>
2018-09-25 12:59:03 +0100

    Implementation of Advanced Role Management, including migration of existing roles

--- api/src/main/java/edu/cornell/mannlib/vivo/auth/policy/InfoContentEntityChecker.java
+++ api/src/main/java/edu/cornell/mannlib/vivo/auth/policy/InfoContentEntityChecker.java
@@ -7,6 +7,7 @@ import java.util.List;
 import edu.cornell.mannlib.vitro.webapp.auth.policy.ifaces.PolicyDecision;
 import edu.cornell.mannlib.vitro.webapp.auth.policy.specialrelationships.RelationshipChecker;
 import edu.cornell.mannlib.vitro.webapp.auth.requestedAction.propstmt.AbstractPropertyStatementAction;
+import org.apache.jena.ontology.OntModel;
 
 /**
  * Does the requested action involve a change to an Info Content Entity that the
@@ -17,13 +18,6 @@ public class InfoContentEntityChecker extends RelationshipChecker {
 	private static final String URI_FEATURES_PROPERTY = NS_CORE + "features";
 	private static final String URI_AUTHORSHIP_TYPE = NS_CORE + "Authorship";
 	private static final String URI_EDITORSHIP_TYPE = NS_CORE + "Editorship";
-
-	private final String[] resourceUris;
-
-	public InfoContentEntityChecker(AbstractPropertyStatementAction action) {
-		super(action.getOntModel());
-		this.resourceUris = action.getResourceUris();
-	}
 
 	/**
 	 * A self-editor is authorized to add, edit, or delete a statement if the
@@ -36,57 +30,45 @@ public class InfoContentEntityChecker extends RelationshipChecker {
 	 *
 	 * 3) is Featured in that Info Content Entity.
 	 */
-	public PolicyDecision isAuthorized(List<String> userUris) {
-		for (String resourceUri : resourceUris) {
-			if (isInfoContentEntity(resourceUri)) {
-				if (anyUrisInCommon(userUris, getUrisOfAuthors(resourceUri))) {
-					return authorizedAuthor(resourceUri);
+	public boolean isRelated(OntModel ontModel, List<String> fromUris, List<String> toUris) {
+		for (String resourceUri : fromUris) {
+			if (isInfoContentEntity(ontModel, resourceUri)) {
+				if (anyUrisInCommon(ontModel, toUris, getUrisOfAuthors(ontModel, resourceUri))) {
+					return true;
 				}
-				if (anyUrisInCommon(userUris, getUrisOfEditors(resourceUri))) {
-					return authorizedEditor(resourceUri);
+				if (anyUrisInCommon(ontModel, toUris, getUrisOfEditors(ontModel, resourceUri))) {
+					return true;
 				}
-				if (anyUrisInCommon(userUris, getUrisOfFeatured(resourceUri))) {
-					return authorizedFeatured(resourceUri);
+				if (anyUrisInCommon(ontModel, toUris, getUrisOfFeatured(ontModel, resourceUri))) {
+					return true;
 				}
 			}
 		}
-		return null;
+
+		return false;
 	}
 
-	private boolean isInfoContentEntity(String resourceUri) {
-		return isResourceOfType(resourceUri, URI_INFO_CONTENT_TYPE);
+	private boolean isInfoContentEntity(OntModel ontModel, String resourceUri) {
+		return isResourceOfType(ontModel, resourceUri, URI_INFO_CONTENT_TYPE);
 	}
 
-	private List<String> getUrisOfEditors(String resourceUri) {
-		List<String> allRelatedUris = getObjectsThroughLinkingNode(resourceUri,
+	private List<String> getUrisOfEditors(OntModel ontModel, String resourceUri) {
+		List<String> allRelatedUris = getObjectsThroughLinkingNode(ontModel, resourceUri,
 				URI_RELATED_BY, URI_EDITORSHIP_TYPE, URI_RELATES);
 		// The editorship relates to the editors and to the resource itself.
 		allRelatedUris.remove(resourceUri);
 		return allRelatedUris;
 	}
 
-	private List<String> getUrisOfFeatured(String resourceUri) {
-		return getObjectsOfProperty(resourceUri, URI_FEATURES_PROPERTY);
+	private List<String> getUrisOfFeatured(OntModel ontModel, String resourceUri) {
+		return getObjectsOfProperty(ontModel, resourceUri, URI_FEATURES_PROPERTY);
 	}
 
-	private List<String> getUrisOfAuthors(String resourceUri) {
-		List<String> allRelatedUris = getObjectsThroughLinkingNode(resourceUri,
+	private List<String> getUrisOfAuthors(OntModel ontModel, String resourceUri) {
+		List<String> allRelatedUris = getObjectsThroughLinkingNode(ontModel, resourceUri,
 				URI_RELATED_BY, URI_AUTHORSHIP_TYPE, URI_RELATES);
 		// The authorship relates to the authors and to the resource itself.
 		allRelatedUris.remove(resourceUri);
 		return allRelatedUris;
 	}
-
-	private PolicyDecision authorizedEditor(String uri) {
-		return authorizedDecision("User is an editor of " + uri);
-	}
-
-	private PolicyDecision authorizedAuthor(String uri) {
-		return authorizedDecision("User is author of " + uri);
-	}
-
-	private PolicyDecision authorizedFeatured(String uri) {
-		return authorizedDecision("User is featured in " + uri);
-	}
-
 }
---
+++ api/src/main/java/edu/cornell/mannlib/vivo/auth/policy/SelfEditorRelationship.java
@@ -0,0 +1,60 @@
+/* $This file is distributed under the terms of the license in LICENSE$ */
+
+package edu.cornell.mannlib.vivo.auth.policy;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import edu.cornell.mannlib.vitro.webapp.utils.RelationshipCheckerRegistry;
+
+/**
+ * Permit self-editors to edit the properties of classes with which they share a
+ * special relationship. So for example:
+ * 
+ * A self-editor may edit properties of an InformationResource for which he is
+ * an author, an editor, or in which he is featured.
+ * 
+ * A self-editor may edit properties of a Project in which he plays a clinical
+ * role.
+ * 
+ * Etc.
+ * 
+ * NOTE: properties or resources which are restricted by namespace or by access
+ * setting will still not be editable, even if this special relationship
+ * applies.
+ * 
+ * NOTE: This could be further generalized by building a list of authorizing
+ * relationships, where each relationship may specify a type of object, a
+ * relating property (or chain of properties), and a text message describing the
+ * relationship (to be used in the decision).
+ */
+public class SelfEditorRelationship {
+	// ----------------------------------------------------------------------
+	// helper classes
+	// ----------------------------------------------------------------------
+
+	/**
+	 * When the system starts up, install the policy. This class must be a
+	 * listener in web.xml
+	 * 
+	 * The CommonIdentifierBundleFactory already creates the IDs we need.
+	 */
+	public static class Setup implements ServletContextListener {
+		@Override
+		public void contextInitialized(ServletContextEvent sce) {
+			ServletContext ctx = sce.getServletContext();
+
+			RelationshipCheckerRegistry.registerRelationshipChecker(new InfoContentEntityChecker());
+			RelationshipCheckerRegistry.registerRelationshipChecker(new GrantChecker());
+			RelationshipCheckerRegistry.registerRelationshipChecker(new ProjectOrServiceChecker());
+			RelationshipCheckerRegistry.registerRelationshipChecker(new PresentationChecker());
+			RelationshipCheckerRegistry.registerRelationshipChecker(new CourseChecker());
+			RelationshipCheckerRegistry.registerRelationshipChecker(new AdvisingRelationshipChecker());
+		}
+
+		@Override
+		public void contextDestroyed(ServletContextEvent sce) { /* nothing */
+		}
+	}
+}
