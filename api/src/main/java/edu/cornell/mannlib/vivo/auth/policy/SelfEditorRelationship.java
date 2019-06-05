e96cf656f03e213dd8e19ba058b4bc39659a663e
Graham Triggs <graham.triggs@tib.eu>
2019-06-05 18:54:32 +0100

    Improve Self Editing Performance

--- api/src/main/java/edu/cornell/mannlib/vivo/auth/policy/SelfEditorRelationship.java
+++ api/src/main/java/edu/cornell/mannlib/vivo/auth/policy/SelfEditorRelationship.java
@@ -5,7 +5,7 @@ package edu.cornell.mannlib.vivo.auth.policy;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import edu.cornell.mannlib.vitro.webapp.utils.RelationshipCheckerRegistry;
+import edu.cornell.mannlib.vitro.webapp.utils.RelationshipChecker;
 
 /**
  * Permit self-editors to edit the properties of classes with which they share a
@@ -42,14 +42,55 @@ public class SelfEditorRelationship {
 	 * The CommonIdentifierBundleFactory already creates the IDs we need.
 	 */
 	public static class Setup implements ServletContextListener {
+		protected static final String NS_CORE = "http://vivoweb.org/ontology/core#";
+		protected static final String NS_OBO = "http://purl.obolibrary.org/obo/";
+		protected static final String URI_RELATES = NS_CORE + "relates";
+		protected static final String URI_RELATED_BY = NS_CORE + "relatedBy";
+		protected static final String URI_BEARER_OF  = NS_OBO + "RO_0000053";
+		protected static final String URI_REALIZED_IN = NS_OBO + "BFO_0000054";
+
+		private static final String URI_ADVISING_RELATIONSHIP_TYPE = NS_CORE + "AdvisingRelationship";
+		private static final String URI_ADVISOR_ROLE = NS_CORE + "AdvisorRole";
+
+		private static final String URI_COURSE_TYPE = NS_CORE + "Course";
+		private static final String URI_TEACHER_ROLE_TYPE = NS_CORE + "TeacherRole";
+
+		private static final String URI_GRANT_TYPE = NS_CORE + "Grant";
+		private static final String URI_PI_ROLE_TYPE = NS_CORE + "PrincipalInvestigatorRole";
+		private static final String URI_CO_PI_ROLE_TYPE = NS_CORE + "CoPrincipalInvestigatorRole";
+
+		private static final String URI_INFO_CONTENT_TYPE = NS_OBO + "IAO_0000030";
+		private static final String URI_AUTHORSHIP_TYPE = NS_CORE + "Authorship";
+		private static final String URI_EDITORSHIP_TYPE = NS_CORE + "Editorship";
+
+		private static final String URI_PRESENTATION_TYPE = NS_CORE + "Presentation";
+		private static final String URI_PRESENTER_ROLE_TYPE = NS_CORE + "PresenterRole";
+
+		private static final String URI_PROJECT_TYPE = NS_CORE + "Project";
+		private static final String URI_SERVICE_TYPE = NS_CORE + "Service";
+		private static final String URI_CLINICAL_ROLE_TYPE = NS_CORE + "ClinicalRole";
+		private static final String URI_CONTRIBUTING_ROLE_PROPERTY = NS_CORE + "contributingRole";
+		private static final String URI_CONTRIBUTES_TO = NS_CORE + "contributesTo";
+
 		@Override
 		public void contextInitialized(ServletContextEvent sce) {
-			RelationshipCheckerRegistry.registerRelationshipChecker(new InfoContentEntityChecker());
-			RelationshipCheckerRegistry.registerRelationshipChecker(new GrantChecker());
-			RelationshipCheckerRegistry.registerRelationshipChecker(new ProjectOrServiceChecker());
-			RelationshipCheckerRegistry.registerRelationshipChecker(new PresentationChecker());
-			RelationshipCheckerRegistry.registerRelationshipChecker(new CourseChecker());
-			RelationshipCheckerRegistry.registerRelationshipChecker(new AdvisingRelationshipChecker());
+			RelationshipChecker.getQueryBuilder()
+				// Advising Relationships
+				.addRelationshipThroughContext(URI_BEARER_OF, URI_ADVISOR_ROLE, URI_RELATED_BY, URI_ADVISING_RELATIONSHIP_TYPE)
+				// Course Relationships
+				.addRelationshipThroughContext(URI_BEARER_OF, URI_TEACHER_ROLE_TYPE, URI_REALIZED_IN, URI_COURSE_TYPE)
+				// Grant Relationships
+				.addRelationshipThroughContext(URI_BEARER_OF, URI_PI_ROLE_TYPE, URI_RELATED_BY, URI_GRANT_TYPE)
+				.addRelationshipThroughContext(URI_BEARER_OF, URI_CO_PI_ROLE_TYPE, URI_RELATED_BY, URI_GRANT_TYPE)
+				// Information Content Relationships
+				.addRelationshipThroughContext(URI_RELATED_BY, URI_AUTHORSHIP_TYPE, URI_RELATES, URI_INFO_CONTENT_TYPE)
+				.addRelationshipThroughContext(URI_RELATED_BY, URI_EDITORSHIP_TYPE, URI_RELATES, URI_INFO_CONTENT_TYPE)
+				// Presentation Relationships
+				.addRelationshipThroughContext(URI_BEARER_OF, URI_PRESENTER_ROLE_TYPE, URI_REALIZED_IN, URI_PRESENTATION_TYPE)
+				// Project Relationships
+				.addRelationshipThroughContext(URI_BEARER_OF, URI_CLINICAL_ROLE_TYPE, URI_CONTRIBUTES_TO, URI_PROJECT_TYPE)
+				.addRelationshipThroughContext(URI_BEARER_OF, URI_CLINICAL_ROLE_TYPE, URI_CONTRIBUTES_TO, URI_SERVICE_TYPE)
+				.finish();
 		}
 
 		@Override
