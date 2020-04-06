1f660f1c1fcfab1918a5e6e739254a71d22d1774
Nicolas D <46490666+nicalico@users.noreply.github.com>
2020-04-06 11:26:26 -0400

    Update AddReviewerRoleToPersonGenerator.java

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/edit/n3editing/configuration/generators/AddReviewerRoleToPersonGenerator.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/edit/n3editing/configuration/generators/AddReviewerRoleToPersonGenerator.java
@@ -8,19 +8,19 @@ import edu.cornell.mannlib.vitro.webapp.edit.n3editing.VTwo.fields.FieldOptions;
 
 public class AddReviewerRoleToPersonGenerator extends AddRoleToPersonTwoStageGenerator {
 
-	private static String OBJECT_VCLASS_URI = "http://purl.org/ontology/bibo/Document";
+    private static String OBJECT_VCLASS_URI = "http://purl.org/ontology/bibo/Document";
 
 	@Override
 	String getTemplate() { return "addReviewerRoleToPerson.ftl"; }
 
-	//The default activityToRolePredicate and roleToActivityPredicates are
+    //The default activityToRolePredicate and roleToActivityPredicates are
 	//correct for this subclass so they don't need to be overwritten
 
-	/*	@Override
+/*	@Override
 	public String getRoleToActivityPredicate(VitroRequest vreq) {
 		return "<http://purl.obolibrary.org/obo/BFO_0000054>";
 	}
-	 */
+*/
 	//role type will always be set based on particular form
 	@Override
 	public String getRoleType() {
@@ -187,13 +187,13 @@ public class AddReviewerRoleToPersonGenerator extends AddRoleToPersonTwoStageGen
 		return false;
 	}
 
-	/*
-	 * Use the methods below to change the date/time precision in the
-	 * custom form associated with this generator. When not used, the
-	 * precision will be YEAR. The other precisons are MONTH, DAY, HOUR,
-	 * MINUTE, TIME and NONE.
-	 */
-	/*
+       /*
+        * Use the methods below to change the date/time precision in the
+        * custom form associated with this generator. When not used, the
+        * precision will be YEAR. The other precisons are MONTH, DAY, HOUR,
+        * MINUTE, TIME and NONE.
+        */
+    /*
         public String getStartDatePrecision() {
             String precision = VitroVocabulary.Precision.MONTH.uri();
     	    return precision;
@@ -203,6 +203,6 @@ public class AddReviewerRoleToPersonGenerator extends AddRoleToPersonTwoStageGen
             String precision = VitroVocabulary.Precision.DAY.uri();
     	    return precision;
         }
-	 */
+    */
 
 }
