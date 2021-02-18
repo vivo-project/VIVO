880ac5b7977ea13e7a981b2ea5e569f1a10be6d2
William Welling <wwelling@library.tamu.edu>
2021-02-18 10:07:19 -0600

    VIVO-1963: Initial docker compose (#223)
    * initial docker compose
    
    * for docker volume use direct tdb filemode
    
    * runtime properties with solr url configured
    
    * move gitattributes to root to ensure scripts stay valid
    
    * fix VIVO dockerfile command permissions
    
    * use public vivoweb/vivo-solr image
    
    * start script improvements
    
    * pass docker compose env to containers
    
    * generate digest.md5 of existing VIVO home
    
    * add initial readme
    
    * reset docker compose env defaults
    
    * minor readme updates

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,22 @@
 FROM tomcat:9-jdk11-openjdk
 
-ENV JAVA_OPTS="${JAVA_OPTS} -Dvivo-dir=/opt/vivo/home/"
+ARG SOLR_URL=http://localhost:8983/solr/vivocore
+ARG VIVO_DIR=/opt/vivo/home
+ARG TDB_FILE_MODE=direct
+
+ENV SOLR_URL=${SOLR_URL}
+ENV JAVA_OPTS="${JAVA_OPTS} -Dvivo-dir=$VIVO_DIR -Dtdb:fileMode=$TDB_FILE_MODE"
 
 RUN mkdir /opt/vivo
 RUN mkdir /opt/vivo/home
 
 COPY ./installer/webapp/target/vivo.war /usr/local/tomcat/webapps/ROOT.war
 
+COPY ./home/src/main/resources/config/default.applicationSetup.n3 /applicationSetup.n3
+COPY ./home/src/main/resources/config/default.runtime.properties /runtime.properties
+
+COPY start.sh /start.sh
+
 EXPOSE 8080
 
-CMD ["catalina.sh", "run"]
+CMD ["/bin/bash", "/start.sh"]
---
+++ start.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+# allow easier debugging with `docker run -e VERBOSE=yes`
+if [[ "$VERBOSE" = "yes" ]]; then
+  set -x
+fi
+
+# allow easier reset home with `docker run -e RESET_HOME=true`
+if [[ "$RESET_HOME" = "true" ]]; then
+  echo 'Clearing VIVO HOME /opt/vivo/home'
+  rm -rf /opt/vivo/home/*
+fi
+
+# ensure home config directory exists
+mkdir -p /opt/vivo/home/config
+
+# generate digest.md5 for existing VIVO home if not already exist
+if [ ! -f /opt/vivo/home/digest.md5 ]; then
+  find /opt/vivo/home -type f | grep -E '^/opt/vivo/home/bin/|^/opt/vivo/home/config/|^/opt/vivo/home/rdf/' | xargs md5sum > /opt/vivo/home/digest.md5
+  echo "Generated digest.md5 for VIVO home"
+  cat /opt/vivo/home/digest.md5
+fi
+
+# only move runtime.properties first time and if it does not already exist in target home directory
+if [ -f /runtime.properties ]; then
+  # template runtime.properties vitro.local.solr.url value to $SOLR_URL value
+  echo "Templating runtime.properties vitro.local.solr.url = $SOLR_URL"
+  sed -i "s,http://localhost:8983/solr/vivocore,$SOLR_URL,g" /runtime.properties
+
+  if [ ! -f /opt/vivo/home/config/runtime.properties ]
+  then
+    echo "First time: moving /runtime.properties to /opt/vivo/home/config/runtime.properties"
+    mv -n /runtime.properties /opt/vivo/home/config/runtime.properties
+  else
+    echo "Using existing /opt/vivo/home/config/runtime.properties"
+  fi
+fi
+
+# only move applicationSetup.n3 first time and if it does not already exist in target home directory
+if [ -f /applicationSetup.n3 ]; then
+  if [ ! -f /opt/vivo/home/config/applicationSetup.n3 ]
+  then
+    echo "First time: moving /applicationSetup.n3 to /opt/vivo/home/config/applicationSetup.n3"
+    mv -n /applicationSetup.n3 /opt/vivo/home/config/applicationSetup.n3
+  else
+    echo "Using existing /opt/vivo/home/config/applicationSetup.n3"
+  fi
+fi
+
+catalina.sh run
