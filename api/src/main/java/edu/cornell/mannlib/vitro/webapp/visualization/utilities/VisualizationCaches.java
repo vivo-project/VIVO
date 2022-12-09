267a90ac93611baa0679a2a873520e0d58793f64
Georgy Litvinov <git@litvinovg.pro>
2022-12-09 11:40:11 +0100

    Improved safety for visualisation cahes queries. (#3790)
    * skip solutions, where label not exists or not a literal
    
    * more safety checks
    
    Co-authored-by: Georgy Litvinov <georgy.litvinov@tib.eu>

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/visualization/utilities/VisualizationCaches.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/visualization/utilities/VisualizationCaches.java
@@ -272,7 +272,11 @@ final public class VisualizationCaches {
                                 @Override
                                 protected void processQuerySolution(QuerySolution qs) {
                                     String org      = qs.getResource("org").getURI();
-                                    String orgLabel = qs.getLiteral("orgLabel").getString();
+                                    Literal labelLiteral = qs.getLiteral("orgLabel");
+                                    if (labelLiteral == null) {
+                                    	return;
+                                    }
+                                    String orgLabel = labelLiteral.getString();
 
                                     map.put(org.intern(), orgLabel.intern());
                                 }
@@ -346,7 +350,11 @@ final public class VisualizationCaches {
                                 @Override
                                 protected void processQuerySolution(QuerySolution qs) {
                                     String org = qs.getResource("org").getURI();
-                                    String typeLabel  = qs.getLiteral("typeLabel").getString();
+                                    Literal labelLiteral = qs.getLiteral("typeLabel");
+                                    if (labelLiteral == null) {
+                                    	return;
+                                    }
+                                    String typeLabel  = labelLiteral.getString();
                                     map.put(org.intern(), typeLabel.intern());
                                 }
                             });
@@ -443,7 +451,11 @@ final public class VisualizationCaches {
                             rdfService.sparqlSelectQuery(query, new ResultSetConsumer() {
                                 protected void processQuerySolution(QuerySolution qs) {
                                     String conceptURI = qs.getResource("concept").getURI().intern();
-                                    String label  = qs.getLiteral("label").getString().intern();
+                                    Literal labelLiteral = qs.getLiteral("label");
+                                    if (labelLiteral == null) {
+                                    	return;
+                                    }
+                                    String label  = labelLiteral.getString().intern();
                                     String labelLower = label.toLowerCase().intern();
 
                                     map.conceptToLabel.put(conceptURI, label);
@@ -537,8 +549,11 @@ final public class VisualizationCaches {
                                 @Override
                                 protected void processQuerySolution(QuerySolution qs) {
                                     String person      = qs.getResource("person").getURI();
-                                    String personLabel = qs.getLiteral("personLabel").getString();
-
+                                    Literal labelLiteral = qs.getLiteral("personLabel");
+                                    if (labelLiteral == null) {
+                                    	return;
+                                    }
+                                    String personLabel  = labelLiteral.getString();
                                     map.put(person.intern(), personLabel.intern());
                                 }
                             });
@@ -571,7 +586,11 @@ final public class VisualizationCaches {
                                 @Override
                                 protected void processQuerySolution(QuerySolution qs) {
                                     String person = qs.getResource("person").getURI();
-                                    String typeLabel  = qs.getLiteral("typeLabel").getString();
+                                    Literal labelLiteral = qs.getLiteral("typeLabel");
+                                    if (labelLiteral == null) {
+                                    	return;
+                                    }
+                                    String typeLabel  = labelLiteral.getString();
                                     map.put(person.intern(), String.valueOf(typeLabel).intern());
                                 }
                             });
@@ -645,8 +664,11 @@ final public class VisualizationCaches {
                                 @Override
                                 protected void processQuerySolution(QuerySolution qs) {
                                     String document      = qs.getResource("document").getURI();
-                                    String journalLabel = qs.getLiteral("journalLabel").getString();
-
+                                    Literal labelLiteral = qs.getLiteral("journalLabel");
+                                    if (labelLiteral == null) {
+                                    	return;
+                                    }
+                                    String journalLabel  = labelLiteral.getString();
                                     map.put(document.intern(), journalLabel.intern());
                                 }
                             });
@@ -679,15 +701,19 @@ final public class VisualizationCaches {
                                 @Override
                                 protected void processQuerySolution(QuerySolution qs) {
                                     String document = qs.getResource("document").getURI();
-                                    String pubDate  = qs.getLiteral("publicationDate").getString();
-                                    if (pubDate != null) {
-                                        DateTime validParsedDateTimeObject = UtilityFunctions
-                                                .getValidParsedDateTimeObject(pubDate);
-
-                                        if (validParsedDateTimeObject != null) {
-                                            map.put(document.intern(), String.valueOf(validParsedDateTimeObject.getYear()).intern());
-                                        }
+                                    Literal dateLiteral = qs.getLiteral("publicationDate");
+                                    if (dateLiteral == null) {
+                                    	return;
                                     }
+                                    String pubDate = dateLiteral.getString();
+                                    if (pubDate == null) {
+                                    	return;
+                                    }
+                                    DateTime validParsedDateTimeObject = UtilityFunctions.getValidParsedDateTimeObject(pubDate);
+                                    if (validParsedDateTimeObject == null) {
+                                    	return;
+                                    }
+                                    map.put(document.intern(), String.valueOf(validParsedDateTimeObject.getYear()).intern());
                                 }
                             });
 
@@ -767,15 +793,19 @@ final public class VisualizationCaches {
                                 @Override
                                 protected void processQuerySolution(QuerySolution qs) {
                                     String grant = qs.getResource("grant").getURI();
-                                    String startDate  = qs.getLiteral("startDateTimeValue").getString();
-                                    if (startDate != null) {
-                                        DateTime validParsedDateTimeObject = UtilityFunctions
-                                                .getValidParsedDateTimeObject(startDate);
-
-                                        if (validParsedDateTimeObject != null) {
-                                            map.put(grant.intern(), String.valueOf(validParsedDateTimeObject.getYear()).intern());
-                                        }
+                                    Literal dateLiteral = qs.getLiteral("startDateTimeValue");
+                                    if (dateLiteral == null) {
+                                        return;
                                     }
+                                    String startDate = dateLiteral.getString();
+                                    if (startDate == null) {
+                                    	return;
+                                    }
+                                    DateTime validParsedDateTimeObject = UtilityFunctions.getValidParsedDateTimeObject(startDate);
+                                    if (validParsedDateTimeObject == null) {
+                                    	return;
+                                    }
+                                    map.put(grant.intern(), String.valueOf(validParsedDateTimeObject.getYear()).intern());
                                 }
                             });
 
@@ -809,15 +839,19 @@ final public class VisualizationCaches {
                                 @Override
                                 protected void processQuerySolution(QuerySolution qs) {
                                     String grant = qs.getResource("grant").getURI();
-                                    String startDate  = qs.getLiteral("startDateTimeValue").getString();
-                                    if (startDate != null) {
-                                        DateTime validParsedDateTimeObject = UtilityFunctions
-                                                .getValidParsedDateTimeObject(startDate);
-
-                                        if (validParsedDateTimeObject != null) {
-                                            map.put(grant.intern(), String.valueOf(validParsedDateTimeObject.getYear()).intern());
-                                        }
+                                    Literal dateLiteral = qs.getLiteral("startDateTimeValue");
+                                    if (dateLiteral == null) {
+                                    	return;
                                     }
+                                    String startDate  = dateLiteral.getString();
+                                    if (startDate == null) {
+                                    	return;
+                                    }
+                                    DateTime validParsedDateTimeObject = UtilityFunctions.getValidParsedDateTimeObject(startDate);
+                                    if (validParsedDateTimeObject == null) {
+                                    	return;
+                                    }
+                                    map.put(grant.intern(), String.valueOf(validParsedDateTimeObject.getYear()).intern());
                                 }
                             });
 
