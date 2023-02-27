4c5ebb3a00af599ad9ad80fb19d1e263f381f47d
William Welling <wwelling@library.tamu.edu>
2023-02-27 16:21:40 -0600

    Docker improvements (WIP)

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,57 @@
+ARG TOMCAT_CONTEXT_PATH=ROOT
+ARG USER_ID=3001
+ARG USER_NAME=vivo
+ARG USER_HOME_DIR=/home/$USER_NAME
+
 FROM tomcat:9-jdk11-openjdk
+ARG TOMCAT_CONTEXT_PATH
+ARG USER_ID
+ARG USER_NAME
+ARG USER_HOME_DIR
+
+ARG VIVO_HOME_DIR=/opt/vivo/home
+ARG TDB_FILE_MODE=direct
+ARG ROOT_USER_ADDRESS=vivo_root@mydomain.edu
+ARG DEFAULT_NAMESPACE=http://vivo.mydomain.edu/individual/
 
 ARG SOLR_URL=http://localhost:8983/solr/vivocore
-ARG VIVO_DIR=/opt/vivo/home
-ARG TDB_FILE_MODE=direct
+ARG SELF_ID_MATCHING_PROPERTY=http://vivo.mydomain.edu/ns
+
+ARG LOAD_SAMPLE_DATA=false
+ARG SAMPLE_DATA_REPO_URL=https://github.com/vivo-project/sample-data.git
+ARG SAMPLE_DATA_BRANCH=main
+ARG SAMPLE_DATA_DIRECTORY=openvivo
+
+ENV JAVA_OPTS="${JAVA_OPTS} -Dvivo-dir=$VIVO_HOME_DIR -Droot-user-address=$ROOT_USER_ADDRESS -Ddefault-namespace=$DEFAULT_NAMESPACE -Dtdb:fileMode=$TDB_FILE_MODE"
+
+ENV VIVO_HOME_DIR=${VIVO_HOME_DIR}
 
 ENV SOLR_URL=${SOLR_URL}
-ENV JAVA_OPTS="${JAVA_OPTS} -Dvivo-dir=$VIVO_DIR -Dtdb:fileMode=$TDB_FILE_MODE"
+ENV SELF_ID_MATCHING_PROPERTY=${SELF_ID_MATCHING_PROPERTY}
 
-RUN mkdir /opt/vivo
-RUN mkdir /opt/vivo/home
+ENV LOAD_SAMPLE_DATA=${LOAD_SAMPLE_DATA}
+ENV SAMPLE_DATA_REPO_URL=${SAMPLE_DATA_REPO_URL}
+ENV SAMPLE_DATA_BRANCH=${SAMPLE_DATA_BRANCH}
+ENV SAMPLE_DATA_DIRECTORY=${SAMPLE_DATA_DIRECTORY}
 
-COPY ./installer/webapp/target/vivo.war /usr/local/tomcat/webapps/ROOT.war
+RUN \
+  apt-get update -y && \
+  apt-get upgrade -y && \
+  apt install git -y
 
-COPY ./home/src/main/resources/config/default.applicationSetup.n3 /applicationSetup.n3
-COPY ./home/src/main/resources/config/default.runtime.properties /runtime.properties
+RUN \
+  addgroup --disabled-password --gid $USER_ID $USER_NAME && \
+  adduser --disabled-password --home $USER_HOME_DIR --uid $USER_ID --gid $USER_ID $USER_NAME
 
-COPY start.sh /start.sh
+RUN mkdir -p $VIVO_HOME_DIR
+
+COPY ./installer/webapp/target/vivo.war /usr/local/tomcat/webapps/$TOMCAT_CONTEXT_PATH.war
+
+COPY ./home/src/main/resources/config/default.applicationSetup.n3 /tmp/applicationSetup.n3
+COPY ./home/src/main/resources/config/default.runtime.properties /tmp/runtime.properties
+
+COPY start.sh /opt/vivo/start.sh
 
 EXPOSE 8080
 
-CMD ["/bin/bash", "/start.sh"]
+CMD ["/bin/bash", "/opt/vivo/start.sh"]
--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+printenv
+
 # allow easier debugging with `docker run -e VERBOSE=yes`
 if [[ "$VERBOSE" = "yes" ]]; then
   set -x
@@ -9,43 +11,86 @@ fi
 
 # allow easier reset home with `docker run -e RESET_HOME=true`
 if [[ "$RESET_HOME" = "true" ]]; then
-  echo 'Clearing VIVO HOME /opt/vivo/home'
-  rm -rf /opt/vivo/home/*
+  echo 'Clearing VIVO HOME $VIVO_HOME_DIR'
+  rm -rf $VIVO_HOME_DIR/*
 fi
 
-# ensure home config directory exists
-mkdir -p /opt/vivo/home/config
+# ensure home config directory exists 
+mkdir -p $VIVO_HOME_DIR/config
 
-# generate digest.md5 for existing VIVO home if not already exist
-if [ ! -f /opt/vivo/home/digest.md5 ]; then
-  find /opt/vivo/home -type f | grep -E '^/opt/vivo/home/bin/|^/opt/vivo/home/config/|^/opt/vivo/home/rdf/' | xargs md5sum > /opt/vivo/home/digest.md5
-  echo "Generated digest.md5 for VIVO home"
-  cat /opt/vivo/home/digest.md5
+# load sample data
+if [[ "$LOAD_SAMPLE_DATA" = "true" ]]; then
+  echo "Cloning sample-data branch $SAMPLE_DATA_BRANCH from $SAMPLE_DATA_REPO_URL"
+  git clone --branch $SAMPLE_DATA_BRANCH $SAMPLE_DATA_REPO_URL sample-data > /dev/null
+
+  # ensure home rdf directory exists 
+  mkdir -p $VIVO_HOME_DIR/rdf
+
+  echo "Loading $SAMPLE_DATA_DIRECTORY/*"
+  cp -r sample-data/$SAMPLE_DATA_DIRECTORY/* $VIVO_HOME_DIR/rdf/.
 fi
 
 # only move runtime.properties first time and if it does not already exist in target home directory
-if [ -f /runtime.properties ]; then
-  # template runtime.properties vitro.local.solr.url value to $SOLR_URL value
-  echo "Templating runtime.properties vitro.local.solr.url = $SOLR_URL"
-  sed -i "s,http://localhost:8983/solr/vivocore,$SOLR_URL,g" /runtime.properties
-
-  if [ ! -f /opt/vivo/home/config/runtime.properties ]
+if [ -f /tmp/runtime.properties ]; then
+  if [ ! -f $VIVO_HOME_DIR/config/runtime.properties ]
   then
-    echo "First time: moving /runtime.properties to /opt/vivo/home/config/runtime.properties"
-    mv -n /runtime.properties /opt/vivo/home/config/runtime.properties
+    # template runtime.properties
+
+    echo "Templating runtime.properties vitro.local.solr.url = $SOLR_URL"
+    sed -i "s,vitro.local.solr.url = http://localhost:8983/solr/vivocore,vitro.local.solr.url = $SOLR_URL,g" /tmp/runtime.properties
+
+    echo "Templating runtime.properties selfEditing.idMatchingProperty = $SELF_ID_MATCHING_PROPERTY"
+    sed -i "s,selfEditing.idMatchingProperty = http://vivo.mydomain.edu/ns#networkId,selfEditing.idMatchingProperty = $SELF_ID_MATCHING_PROPERTY,g" /tmp/runtime.properties
+
+    if [[ ! -z "${EMAIL_SMTP_HOST}" ]]; then
+      echo "Templating runtime.properties email.smtpHost = $EMAIL_SMTP_HOST"
+      sed -i "s,  # email.smtpHost = smtp.mydomain.edu,email.smtpHost = $EMAIL_SMTP_HOST,g" /tmp/runtime.properties
+    fi
+    if [[ ! -z "${EMAIL_PORT}" ]]; then
+      echo "Templating runtime.properties email.port = $EMAIL_PORT"
+      sed -i "s,  # email.port = 25 or 587,email.port = $EMAIL_PORT,g" /tmp/runtime.properties
+    fi
+    if [[ ! -z "${EMAIL_USERNAME}" ]]; then
+      echo "Templating runtime.properties email.username = $EMAIL_USERNAME"
+      sed -i "s,  # email.username = vivtroAdmin@mydomain.edu,email.username = $EMAIL_USERNAME,g" /tmp/runtime.properties
+    fi
+    if [[ ! -z "${EMAIL_PASSWORD}" ]]; then
+      echo "Templating runtime.properties email.password = ***"
+      sed -i "s,  # email.password = secret,email.password = $EMAIL_PASSWORD,g" /tmp/runtime.properties
+    fi
+    if [[ ! -z "${EMAIL_REPLY_TO}" ]]; then
+      echo "Templating runtime.properties email.replyTo = $EMAIL_REPLY_TO"
+      sed -i "s,  # email.replyTo = vitroAdmin@mydomain.edu,email.replyTo = $EMAIL_REPLY_TO,g" /tmp/runtime.properties
+    fi
+
+    if [[ ! -z "${LANGUAGE_FILTER_ENABLED}" ]]; then
+      echo "Templating runtime.properties RDFService.languageFilter = $LANGUAGE_FILTER_ENABLED"
+      sed -i "s,# RDFService.languageFilter = false,RDFService.languageFilter = $LANGUAGE_FILTER_ENABLED,g" /tmp/runtime.properties
+    fi
+    if [[ ! -z "${FORCE_LOCALE}" ]]; then
+      echo "Templating runtime.properties languages.forceLocale = $FORCE_LOCALE"
+      sed -i "s,# languages.forceLocale = en_US,languages.forceLocale = $FORCE_LOCALE,g" /tmp/runtime.properties
+    fi
+    if [[ ! -z "${SELECTABLE_LOCALES}" ]]; then
+      echo "Templating runtime.properties languages.selectableLocales = $SELECTABLE_LOCALES"
+      sed -i "s/# languages.selectableLocales = en_US, es_GO/languages.selectableLocales = $SELECTABLE_LOCALES/g" /tmp/runtime.properties
+    fi
+
+    echo "First time: moving /tmp/runtime.properties to /$VIVO_HOME_DIR/config/runtime.properties"
+    mv -n /tmp/runtime.properties $VIVO_HOME_DIR/config/runtime.properties
   else
-    echo "Using existing /opt/vivo/home/config/runtime.properties"
+    echo "Using existing $VIVO_HOME_DIR/config/runtime.properties"
   fi
 fi
 
 # only move applicationSetup.n3 first time and if it does not already exist in target home directory
-if [ -f /applicationSetup.n3 ]; then
-  if [ ! -f /opt/vivo/home/config/applicationSetup.n3 ]
+if [ -f /tmp/applicationSetup.n3 ]; then
+  if [ ! -f $VIVO_HOME_DIR/config/applicationSetup.n3 ]
   then
-    echo "First time: moving /applicationSetup.n3 to /opt/vivo/home/config/applicationSetup.n3"
-    mv -n /applicationSetup.n3 /opt/vivo/home/config/applicationSetup.n3
+    echo "First time: moving /tmp/applicationSetup.n3 to $VIVO_HOME_DIR/config/applicationSetup.n3"
+    mv -n /tmp/applicationSetup.n3 $VIVO_HOME_DIR/config/applicationSetup.n3
   else
-    echo "Using existing /opt/vivo/home/config/applicationSetup.n3"
+    echo "Using existing $VIVO_HOME_DIR/config/applicationSetup.n3"
   fi
 fi
 
