7badf5fe1ac256fa17821204b5d162a5c32e2d15
Milos Popovic <misa01523@gmail.com>
2024-04-11 22:37:29 +0200

    Replaced ImageSparkline with LineChart

--- webapp/src/main/webapp/js/visualization/visualization-helper-functions.js
+++ webapp/src/main/webapp/js/visualization/visualization-helper-functions.js
@@ -46,56 +46,6 @@ function extendedEncodeDataForChartURL(arrVals, maxVal) {
     return chartData;
 }
 
-/**
- * This will be used for getting images directly from the secure https://quickchart.io
- * instead of http://charts.apis.google.com which currently throws security warnings.
- *
- * see http://code.google.com/apis/chart/docs/chart_params.html FOR chart parameters
- * see http://code.google.com/apis/chart/docs/data_formats.html FOR how to encode data
- *
- * sample constructed URL - https://quickchart.io/chart?cht=ls&chs=148x58&chdlp=r&chco=3399CC&chd=e%3AW2ttpJbb..ttgAbbNtAA
- */
-function constructVisualizationURLForSparkline(dataString, visualizationOptions) {
-
-	/*
-	 * Since we are directly going to use this URL in img tag, we are supposed to enocde "&"
-	 * update: But since we are directly using it in an Image creating function we dont need to encode it.
-	*/
-    //var parameterDifferentiator = "&amp;";
-    var parameterDifferentiator = "&";
-
-    var rootGoogleChartAPI_URL = "https://quickchart.io/chart?";
-
-    /*
-     * cht=ls indicates chart of type "line chart sparklines".
-     * see http://code.google.com/apis/chart/docs/gallery/chart_gall.html
-	*/
-    var chartType = "cht=" + visualizationOptions.chartType;
-
-    /*
-     * It seems google reduces 2px from width & height before rendering the actual image.
-     * We will do the same.
-	*/
-    var chartSize = "chs=" + (visualizationOptions.width - 2) + "x" + (visualizationOptions.height - 2);
-
-    /*
-     * It means that legend, if present, is to be displayed to the right of the chart,
-     * legend entries in a vertical column.
-	*/
-    var chartLabelPosition = "chdlp=" + visualizationOptions.chartLabel;
-
-    /*
-     * Color of the sparkline.
-	*/
-    var chartColor = "chco=" + visualizationOptions.color;
-
-    return rootGoogleChartAPI_URL + chartType + parameterDifferentiator
-    			+ chartSize + parameterDifferentiator
-    			+ chartLabelPosition + parameterDifferentiator
-    			+ chartColor + parameterDifferentiator
-    			+ "chd=" + dataString
-}
-
 /*
  * In IE trim() is not supported.
  * */
